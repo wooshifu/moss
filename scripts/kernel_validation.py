ce0d99946249cbe8c33e7c42f70fccaad3064238
shifu.wu <shifu.wu@outlook.com>
2026-09-23 12:13:29 +0800

    [test] cover concurrent fork clone and unmap

--- scripts/kernel_validation.py
+++ scripts/kernel_validation.py
@@ -72,7 +72,7 @@ CATALOG = {
     "containers.smp": ["interleaving"],
     "vfs.smp": ["shared_references"],
     "mm.lifetime": ["held_readers", "hardware_root", "kernel_root"],
-    "mm.concurrent": ["cow_fault", "demand_fault", "fault_unmap", "fault_fork"],
+    "mm.concurrent": ["cow_fault", "demand_fault", "fault_unmap", "fault_fork", "fork_unmap"],
     "mm.uaccess": ["copy_unmap", "copy_fork"],
     "mm.tlb_broadcast": [
         "local_remap",
